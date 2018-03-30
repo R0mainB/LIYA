2391e7c4a0aa97fe910724d3fc6f180c4719a513
R0mainB <romain.bonhomme1@gmail.com>
2018-03-30 16:46:17 +0200

    Menu de l'application

--- app/src/main/java/adventure/liya/mgr/com/liya/AccueilActivity.java
+++ app/src/main/java/adventure/liya/mgr/com/liya/AccueilActivity.java
@@ -2,6 +2,7 @@ package adventure.liya.mgr.com.liya;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class AccueilActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+
     }
 }
