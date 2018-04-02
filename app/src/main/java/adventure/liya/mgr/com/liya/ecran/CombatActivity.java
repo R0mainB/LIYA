0b90d4c3408cb0b27a227ea427f6d59cf6fdcdc4
BuildTools <unconfigured@null.spigotmc.org>
2018-04-02 22:38:27 +0200

    Création écran de combat toujours en vomis

---
+++ app/src/main/java/adventure/liya/mgr/com/liya/ecran/CombatActivity.java
@@ -0,0 +1,15 @@
+package adventure.liya.mgr.com.liya.ecran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import adventure.liya.mgr.com.liya.R;
+
+public class CombatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_combat);
+    }
+}
