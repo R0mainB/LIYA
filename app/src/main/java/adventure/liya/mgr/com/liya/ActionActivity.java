49eaeaf60d70054c650dbad47a96fef10a223995
Maxblandin <maxence.blandin@gmail.com>
2018-04-02 20:50:02 +0200

    Ajout de l'écran action

---
+++ app/src/main/java/adventure/liya/mgr/com/liya/ActionActivity.java
@@ -0,0 +1,13 @@
+package adventure.liya.mgr.com.liya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action);
+    }
+}
