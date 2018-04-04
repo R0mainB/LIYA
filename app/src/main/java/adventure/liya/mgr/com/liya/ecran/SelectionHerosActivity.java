5972be2cc01ed353cfb14b15a99f1360acd3e3f8
maxblandin <maxence.blandin@gmail.com>
2018-04-04 14:11:55 +0200

    Ajout ecran selection personnage

---
+++ app/src/main/java/adventure/liya/mgr/com/liya/ecran/SelectionHerosActivity.java
@@ -0,0 +1,15 @@
+package adventure.liya.mgr.com.liya.ecran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import adventure.liya.mgr.com.liya.R;
+
+public class SelectionHerosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_heros);
+    }
+}
